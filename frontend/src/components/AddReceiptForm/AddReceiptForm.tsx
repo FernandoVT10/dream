b9bf6142245ee9eb8e6e10b9408d71d4521f1b32
Infinity <infinity@thinkpad.domain_not_set.invalid>
2024-10-18 12:58:13 -0600

    Better way to handle icons

--- frontend/src/components/AddReceiptForm/AddReceiptForm.tsx
+++ frontend/src/components/AddReceiptForm/AddReceiptForm.tsx
@@ -1,5 +1,6 @@
 import { useState, useReducer } from "react";
 import { API_URL } from "../../constants";
+import { XIcon } from "../../icons";
 
 import Notifications from "../../Notifications";
 
@@ -140,21 +141,7 @@ function MixForm({ mixForm, removeForm, updateForm }: MixFormProps) {
           className={styles.deleteBtn}
           onClick={() => removeForm(mixForm.id)}
         >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="18"
-            height="18"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          >
-            <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-            <path d="M18 6l-12 12" />
-            <path d="M6 6l12 12" />
-          </svg>
+          <XIcon size={18} strokeWidth={4}/>
         </button>
       </div>
     </div>
