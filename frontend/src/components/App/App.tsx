904e154db3f846d94c35d43cbb6fea7dee241863
Fernando Vaca Tamayo <vacatamayo321@gmail.com>
2024-10-23 16:46:35 -0600

    Add info components to the main table

--- frontend/src/components/App/App.tsx
+++ frontend/src/components/App/App.tsx
@@ -8,24 +8,52 @@ import Filters from "../Filters";
 import AddReceiptForm from "../AddReceiptForm";
 import Receipts from "../Receipts";
 import Api from "../../Api";
+import Spinner from "../Spinner";
 
 import styles from "./App.module.scss";
 
 function App() {
+  const [receipts, setReceipts] = useState<Receipt[]>([]);
+  const [loading, setLoading] = useState(false);
+
   const addReceiptModal = useModal();
 
-  const [receipts, setReceipts] = useState<Receipt[]>([]);
-
   const loadReceipts = async (search?: string): Promise<void> => {
+    setLoading(true);
+
     try {
       setReceipts(await Api.getReceipts(search));
     } catch {
       Notifications.error("There was an error with the server.");
     }
+
+    setLoading(false);
   };
 
   const addReceipt = (receipt: Receipt) => {
     setReceipts([receipt, ...receipts]);
+  };
+
+  const getReceiptsComponent = () => {
+    if(loading) {
+      return (
+        <div className={styles.loader}>
+          <Spinner size={50}/>
+        </div>
+      );
+    }
+
+    if(!receipts.length) {
+      return (
+        <div className={styles.infoMessage}>
+          <p className={styles.text}>No receipts found</p>
+        </div>
+      );
+    }
+
+    return (
+      <Receipts receipts={receipts} setReceipts={setReceipts}/>
+    );
   };
 
   return (
@@ -33,13 +61,13 @@ function App() {
       <Modal title="Add Receipt" modal={addReceiptModal}>
         <AddReceiptForm hideModal={addReceiptModal.hide} addReceiptToState={addReceipt}/>
       </Modal>
+
       <div className={styles.app}>
         <Filters
           loadReceipts={loadReceipts}
           showReceiptModal={() => addReceiptModal.show()}
         />
-
-        <Receipts receipts={receipts} setReceipts={setReceipts}/>
+        {getReceiptsComponent()}
       </div>
     </>
   )
