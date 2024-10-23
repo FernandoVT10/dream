aa94d5331ec3a9f3e03ba69d1caf8cd0ef9b6e61
Fernando Vaca Tamayo <vacatamayo321@gmail.com>
2024-10-23 17:29:48 -0600

    Redesign kind selector

--- frontend/src/components/Filters/Filters.tsx
+++ frontend/src/components/Filters/Filters.tsx
@@ -2,17 +2,18 @@ import { useState, useEffect } from "react";
 
 import styles from "./Filters.module.scss";
 
+const KIND_LIST = [
+  { name: "Raspberries", value: "raspberry" },
+  { name: "Corn", value: "corn" },
+  { name: "Strawberries", value: "strawberry" },
+];
+
+const SELECTED_KIND = KIND_LIST[0].value;
+
 const STATUS_LIST = [
   { name: "All", value: "all" },
   { name: "Pending", value: "pending" },
   { name: "Delivered", value: "delivered" },
-];
-
-const KIND_LIST = [
-  { name: "All", value: "all" },
-  { name: "Raspberries", value: "raspberry" },
-  { name: "Corn", value: "corn" },
-  { name: "Strawberries", value: "strawberry" },
 ];
 
 const SEARCH_BY_LIST = [
@@ -33,22 +34,20 @@ const getValue = (cb: (value: string) => void) => {
 };
 
 function Filters({ loadReceipts, showReceiptModal }: FiltersProps) {
+  const [selectedKind, setSelectedKind] = useState(SELECTED_KIND);
   const [searchBy, setSearchBy] = useState("folio");
   const [search, setSearch] = useState("");
-  const [kind, setKind] = useState(KIND_LIST[0].value);
   const [status, setStatus] = useState(STATUS_LIST[1].value);
 
   useEffect(() => {
-    let query = "";
-
     // the query or search syntax is in the form: "[filter_name]: [value];"
     // each filter term is separated by a semicolon
+    let query = `kind:${selectedKind};`;
     if(search) query = `${searchBy}:${search};`;
-    if(kind !== "all") query += `kind:${kind};`;
     if(status !== "all") query += `status:${status};`;
 
     loadReceipts(query);
-  }, [search, kind, status]);
+  }, [selectedKind, search, status]);
 
   const handleSearchBy = (e: React.ChangeEvent<HTMLSelectElement>): void => {
     setSearchBy(e.target.value);
@@ -56,64 +55,70 @@ function Filters({ loadReceipts, showReceiptModal }: FiltersProps) {
   };
 
   return (
-    <div className={styles.filters}>
-      <div className={styles.searchBar}>
-        <select className={styles.select} value={searchBy} onChange={handleSearchBy}>
-          {SEARCH_BY_LIST.map(({ value, name }) => {
-            return (
-              <option value={value} key={value}>{name}</option>
-            );
-          })}
-        </select>
+    <>
+      <div className={styles.kindSelector}>
+        {KIND_LIST.map(kind => {
+          const activeClass = selectedKind === kind.value && styles.active;
 
-        { searchBy === "date" ? (
-          <input
-            type="date"
-            className={`${styles.input} ${styles.dateInput}`}
-            value={search}
-            onChange={getValue(v => setSearch(v))}
-          />
-        ) : (
-          <input
-            type="text"
-            className={styles.input}
-            placeholder={`Enter a ${searchBy.toLowerCase()} to filter`}
-            value={search}
-            onChange={getValue(v => setSearch(v))}
-          />
-        )}
+          return (
+            <button
+              className={`${styles.kind} ${activeClass}`}
+              onClick={() => setSelectedKind(kind.value)}
+              key={kind.value}
+            >
+              {kind.name}
+            </button>
+          );
+        })}
       </div>
 
-      <div className={styles.extraFilters}>
-        <select
-          className={styles.select}
-          value={status}
-          onChange={getValue(v => setStatus(v))}
-        >
-          {STATUS_LIST.map(status => {
-            return (
-              <option key={status.name} value={status.value}>{status.name}</option>
-            );
-          })}
-        </select>
+      <div className={styles.filters}>
+        <div className={styles.searchBar}>
+          <select className={styles.select} value={searchBy} onChange={handleSearchBy}>
+            {SEARCH_BY_LIST.map(({ value, name }) => {
+              return (
+                <option value={value} key={value}>{name}</option>
+              );
+            })}
+          </select>
 
-        <select
-          className={styles.select}
-          value={kind}
-          onChange={getValue(v => setKind(v))}
-        >
-          {KIND_LIST.map(kind => {
-            return (
-              <option key={kind.name} value={kind.value}>{kind.name}</option>
-            );
-          })}
-        </select>
+          { searchBy === "date" ? (
+            <input
+              type="date"
+              className={`${styles.input} ${styles.dateInput}`}
+              value={search}
+              onChange={getValue(v => setSearch(v))}
+            />
+          ) : (
+              <input
+                type="text"
+                className={styles.input}
+                placeholder={`Enter a ${searchBy.toLowerCase()} to filter`}
+                value={search}
+                onChange={getValue(v => setSearch(v))}
+              />
+            )}
+        </div>
 
-        <button className={styles.addReceiptBtn} onClick={showReceiptModal}>
-          Add Receipt
-        </button>
+        <div className={styles.extraFilters}>
+          <select
+            className={styles.select}
+            value={status}
+            onChange={getValue(v => setStatus(v))}
+          >
+            {STATUS_LIST.map(status => {
+              return (
+                <option key={status.name} value={status.value}>{status.name}</option>
+              );
+            })}
+          </select>
+
+          <button className={styles.addReceiptBtn} onClick={showReceiptModal}>
+            Add Receipt
+          </button>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
